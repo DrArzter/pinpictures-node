7fd353f1a2b08669cfd8e5904eb4dc3d3edcbef9
DrArzter <chapegarostislav@gmail.com>
2024-10-01 10:36:22 +0200

    Minor changes.

--- middlewares/imageMiddleware.js
+++ middlewares/imageMiddleware.js
@@ -2,7 +2,6 @@ const multer = require('multer');
 const path = require('path');
 
 module.exports = (req, res, next) => {
-  // Настраиваем хранение файлов с помощью multer
   const storage = multer.memoryStorage();
   const fileFilter = (req, file, cb) => {
     const fileTypes = /jpeg|jpg|png|gif/;
@@ -16,27 +15,21 @@ module.exports = (req, res, next) => {
     }
   };
 
-  // Создаем multer middleware
   const upload = multer({
     storage: storage,
     fileFilter: fileFilter,
-    limits: { fileSize: 10 * 1024 * 1024 } // Ограничение на размер файла: 10MB
+    limits: { fileSize: 10 * 1024 * 1024 }
   });
 
-  // Middleware для загрузки нескольких изображений
   const uploadImagesMiddleware = upload.array('files', 10);
 
-  // Используем middleware для загрузки файлов
   uploadImagesMiddleware(req, res, (err) => {
     if (err instanceof multer.MulterError) {
-      // Обработка ошибок multer (например, превышение размера файла)
       return res.status(400).json({ error: err.message });
     } else if (err) {
-      // Обработка других ошибок
       return res.status(400).json({ error: err.message });
     }
 
-    // Если ошибок нет, продолжаем к следующему middleware
     next();
   });
 };
--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -2,13 +2,14 @@ const express = require('express');
 const router = express.Router();
 const chatController = require('../controllers/chatController');
 const authMiddleware = require('../middlewares/authMiddleware');
+const validate = require('../middlewares/validator');
 const multer = require('multer');
 const uploadComment = multer({ dest: 'uploads/chats' });
 
-router.get('/', authMiddleware, chatController.getChatsByUserId);
-router.post('/', authMiddleware, chatController.createChat);
-router.post('/messages/:id', authMiddleware, chatController.uploadMessage);
-router.get('/:id', authMiddleware, chatController.getChatById);
-router.get('/messages/:id', authMiddleware, chatController.getMessages);
+router.get('/', validate, authMiddleware, chatController.getChatsByUserId);
+router.post('/', validate, authMiddleware, chatController.createChat);
+router.post('/messages/:id', validate, authMiddleware, chatController.uploadMessage);
+router.get('/:id', validate, authMiddleware, chatController.getChatById);
+router.get('/messages/:id', validate, authMiddleware, chatController.getMessages);
 
 module.exports = router;
